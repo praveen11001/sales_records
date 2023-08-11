d3077a416d379b6f08dbca7c63e28527b38668ad
praveen11001 <139953845+praveen11001@users.noreply.github.com>
2023-08-11 17:48:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import streamlit as st
 import pandas as pd
 import snowflake.connector as sf
 
-st.title("Connecting Snowflake Data to Streamlit Web App")
-st.header('The Product Data')
-st.write('The discount price was given in the "discount_price" column, and they are marked with color background to provide easy data visualization insight.')
+st.title("Insight Employee Data App")
+st.write('This app helps you take a look at employee data with selecting column at your ease.')
 
 sidebar = st.sidebar
 
@@ -16,19 +15,14 @@ def connect_to_snowflake(acct, u, pd, rl, wh, db):
     st.session_state['is_ready'] = True
     return cs
 
-# Function to apply highlighting
-def highlight_discount_price(value):
-    if value == 0:
-        return 'background-color: #8FBC8F'  # Green
-    else:
-        return 'background-color: #FF6347'  # Red
-
 # Function to fetch data from Snowflake
-def get_data():
-    query = 'select * from raw_data.refined_products.products;'
+def get_data(query):
     results = st.session_state['snow_conn'].execute(query)
     results = st.session_state['snow_conn'].fetchall()
     return results
+
+# Initialize an empty DataFrame to store the selected employee details
+selected_data_df = pd.DataFrame()
 
 with sidebar:
     account = st.text_input('Account')
@@ -44,13 +38,32 @@ if 'is_ready' not in st.session_state:
     st.session_state['is_ready'] = False
 
 if st.session_state['is_ready'] == True:
-    data = get_data()
+    # Fetch column names from Snowflake table
+    column_data = get_data('select * from raw_data.raw_schema.employee LIMIT 1;')
+    
+    # Extract the column names from the result
+    column_names = [desc[0] for desc in st.session_state['snow_conn'].description]
 
-    # Convert fetched data to a Pandas DataFrame
-    df = pd.DataFrame(data, columns=['ID', 'NAME', 'CATEGORY', 'QUANTITY', 'Amount', 'Discount_price', 'Purchased_at'])
+    # Display the multiselect widget for column names
+    selected_columns = st.multiselect("Pick columns to show:", column_names)
+    
+    # Add a checkbox to enable/disable filtering columns with only null data
+    filter_null_columns = st.checkbox("Show Columns with Null Data")
+    
+    # Fetch all rows of selected columns from Snowflake table
+    if selected_columns:
+        query = f"select {', '.join(selected_columns)} from raw_data.raw_schema.employee;"
+        selected_data = get_data(query)
+        selected_data_df = pd.DataFrame(selected_data, columns=selected_columns)
+        
+        # Filter out columns with only null data if the checkbox is enabled
+        if filter_null_columns:
+            non_null_columns = selected_data_df.columns[selected_data_df.notnull().any()]
+            selected_data_df = selected_data_df[non_null_columns]
 
-    # Apply highlighting to the 'Discount_price' column based on the value
-    df_styled = df.style.applymap(highlight_discount_price, subset=['Discount_price'])
-
-    # Display the styled DataFrame using st.write
-    st.write(df_styled)
+# Display the accumulated data table
+st.write("Accumulated Data:")
+if not selected_data_df.empty:
+    st.write(selected_data_df)
+else:
+    st.write("No data selected.")
